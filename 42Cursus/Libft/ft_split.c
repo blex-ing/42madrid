0c384dfb4bfd01da6566a6cb59c3810a5b7029fe
Michael Espinosa Males <47524846+blex-ing@users.noreply.github.com>
2023-11-15 14:00:51 +0000

    ADD ft_split

--- 42Cursus/Libft/ft_split.c
+++ 42Cursus/Libft/ft_split.c
@@ -1,72 +1,72 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aleespin <aleespin@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/31 19:11:19 by aleespin          #+#    #+#             */
-/*   Updated: 2023/10/31 19:51:16 by aleespin         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 #include <stdlib.h>
 
+static int	ft_count_words(const char *s, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+static char	*ft_strndup(const char *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = (char *)malloc(sizeof(char) * (n + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 char	**ft_split(const char *s, char c)
 {
-    char *sentinela;
-    char **big_r;
-    int num_c;
-    sentinela = (char *)s;
-    num_c = 0;
-    while (*sentinela)
-    {
-        if (*sentinela  == c)
-            num_c++;
-    }
-    big_r = (char **)malloc(sizeof(char *) * (num_c + 1));
-    if(big_r == NULL)
-        return (NULL);
-    ft_bzero(big_r,0);
-    int cont = 0;
-    char *punt_c;
-    while (big_r[cont] != '\0')
-    {
-        punt_c =ft_strchr(sentinela, c);
-        big_r[cont] = (char *)malloc((punt_c - sentinela) * sizeof(char));
-        sentinela = punt_c + 1;
-        
-    }
+	char	**arr;
+	int		i;
+	int		j;
 
-    return (big_r);
-    
-
-    
-
-	// char	**matrix;
-	// size_t	len;
-	// size_t	i;
-	// size_t	sl;
-
-	// i = 0;
-	// sl = 0;
-	// len = ft_numstring(s, c);
-	// matrix = (char **)malloc(sizeof(char *) * (len + 1));
-	// if (!matrix)
-	// 	return (NULL);
-	// while (i < len)
-	// {
-	// 	while (*s == c)
-	// 		s++;
-	// 	sl = ft_numchar((const char *)s, c);
-	// 	matrix[i] = (char *)malloc(sizeof(char) * sl + 1);
-	// 	if (!matrix[i])
-	// 		return (ft_free_matrix((const char **)matrix, len));
-	// 	ft_strlcpy(matrix[i], s, sl + 1);
-	// 	s = (ft_strchr(s, (int)c));
-	// 	i++;
-	// }
-	// matrix[i] = 0;
-	// return (matrix);
+	if (!s)
+		return (NULL);
+	arr = (char **)malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			j = 0;
+			while (s[j] && s[j] != c)
+				j++;
+			arr[i] = ft_strndup(s, j);
+			if (!arr[i])
+				return (NULL);
+			i++;
+			s += j;
+		}
+		else
+			s++;
+	}
+	arr[i] = NULL;
+	return (arr);
 }
+
