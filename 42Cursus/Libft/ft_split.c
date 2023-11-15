d306d168584fb3cb47f8f22c86c7d5ea253d997a
blex-ing <alex.espinosa@edu.uah.es>
2023-11-15 18:08:09 +0100

    split

--- 42Cursus/Libft/ft_split.c
+++ 42Cursus/Libft/ft_split.c
@@ -12,34 +12,91 @@
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
 
 char	**ft_split(const char *s, char c)
 {
-    char *sentinela;
-    char **big_r;
-    int num_c;
-    sentinela = (char *)s;
-    num_c = 0;
-    while (*sentinela)
+      if(ft_strlen(s) != '\0')
     {
-        if (*sentinela  == c)
-            num_c++;
-    }
-    big_r = (char **)malloc(sizeof(char *) * (num_c + 1));
-    if(big_r == NULL)
-        return (NULL);
-    ft_bzero(big_r,0);
-    int cont = 0;
-    char *punt_c;
-    while (big_r[cont] != '\0')
-    {
-        punt_c =ft_strchr(sentinela, c);
-        big_r[cont] = (char *)malloc((punt_c - sentinela) * sizeof(char));
-        sentinela = punt_c + 1;
-        
-    }
+        char *partida = (char *)s;
+        // char *split;
+        char **cadena_split;
+       // = (char **)malloc(sizeof(char *) * 10);
+        int indice = 0;
+        while (*partida != '\0')
+        {
+            if (*partida != c && (*(partida + 1) == c))
+                indice++;
+            partida++;
+        }
+        if(indice > 0)
+        {
+            cadena_split = (char **)malloc(sizeof(char *) * (indice + 1));
+            if(cadena_split == NULL)
+                return (NULL);
+            // printf(" \n#cadena split: %d\n",indice);
+            // printf("#\ncadena split + 1: %d\n", indice + 1);
+            partida = (char *)s;
+           // char *aux_s = (char *)s;
+            int indice_1 = 0;
+            int x = 0;
+            
+            while (*partida != '\0')
+            {
+                if (*(s + x) != c)
+                {
+                    int y = 0;
+                    while(*(s + x + y) != c)
+                        y++;
+                    char *split;
+                    split = ft_substr(s, (unsigned int) x, (size_t) (x + y));
+                    // printf("split: %s\n", split);
+                    cadena_split[indice_1] = ft_strdup(split);
+                    //aux_s = partida + 1;
+                    partida = (partida + y);
+                    indice_1++;
+                }
+                partida++;
+            }
+            return (cadena_split);
+        }else
+        {
+            char **matriz;
+            matriz = (char **)malloc(sizeof(char));
+           // *matriz = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+            *matriz = ft_substr(s, 0, ft_strlen(s));
+            *(matriz + 1) = '\0';
 
-    return (big_r);
+            return (matriz);
+        }
+    }
+    return ((char **)s);
+    // char *sentinela;
+    // char **big_r;
+    // int num_c;
+    // sentinela = (char *)s;
+    // num_c = 0;
+    // while (*sentinela)
+    // {
+    //     if (*sentinela  == c)
+    //         num_c++;
+    // }
+    // big_r = (char **)malloc(sizeof(char *) * (num_c + 1));
+    // if(big_r == NULL)
+    //     return (NULL);
+    // ft_bzero(big_r,0);
+    // int cont = 0;
+    // char *punt_c;
+    // while (big_r[cont] != '\0')
+    // {
+    //     punt_c =ft_strchr(sentinela, c);
+    //     big_r[cont] = (char *)malloc((punt_c - sentinela) * sizeof(char));
+    //     sentinela = punt_c + 1;
+        
+    // }
+
+    // return (big_r);
     
 
     
